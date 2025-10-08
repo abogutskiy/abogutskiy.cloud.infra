e15a3e76e0d1d5c92d6c16d6df39a143d257c18c
abogutksiy <bogutskiy.alexandr@gmail.com>
2025-10-08 17:28:32 +0200

    Fix deploy

--- abogutskiy.com/deploy.sh
+++ abogutskiy.com/deploy.sh
@@ -5,11 +5,17 @@ DIR=$(dirname "$SCRIPT")
 
 sudo mkdir -p /etc/docker/abogutskiy.com/
 sudo cp compose.yaml /etc/docker/abogutskiy.com/
+sudo touch /etc/docker/abogutskiy.com/.env
 sudo chown -R www-data:www-data /etc/docker/abogutskiy.com/
 sudo cp abogutskiy.com.service /etc/systemd/system/
 
+NAME=abogutskiy.com
+SERVICE=$NAME.service
+
+IMAGE_ID=`docker image inspect -f '{{.Id}}' $NAME:latest`
+docker build . -t $NAME:latest
+
 sudo systemctl daemon-reload
-SERVICE=abogutskiy.com.service
 sudo systemctl enable $SERVICE
 
 if systemctl is-active --quiet $SERVICE; then
@@ -19,4 +25,8 @@ else
     echo "$SERVICE is not running, starting..."
     sudo systemctl start $SERVICE
 fi
+
+
+docker rm -f $(docker ps -a --filter "ancestor=$IMAGE_ID" --format "{{.ID}}") && docker image rm -f $IMAGE_ID
+
 
